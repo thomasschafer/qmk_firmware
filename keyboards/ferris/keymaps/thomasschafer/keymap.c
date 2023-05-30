fadea975caee5e1f55badb937f60b24800f128b6
thomasschafer <thomasschafer97@gmail.com>
2023-05-30 18:17:04 +0100

    swap symbols on second and third rows of symbols layer to remove need for tap dance

--- keyboards/ferris/keymaps/thomasschafer/keymap.c
+++ keyboards/ferris/keymaps/thomasschafer/keymap.c
@@ -1,15 +1,5 @@
 #include QMK_KEYBOARD_H
 
-enum {
-  TD_TILD_LSFT = 0,
-  TD_UNDS_LALT,
-  TD_PLUS_LGUI,
-  TD_DQUO_RGUI,
-  TD_PIPE_RALT,
-  TD_LCBR_RCTL,
-  TD_RCBR_RSFT
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_split_3x5_2(
 		KC_Q, KC_W, KC_E, KC_R, KC_T,
@@ -28,11 +18,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,
 		KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
 
-		TD(TD_TILD_LSFT), KC_LCTL, TD(TD_UNDS_LALT), TD(TD_PLUS_LGUI), KC_NO,
-		KC_NO, TD(TD_DQUO_RGUI), TD(TD_PIPE_RALT), TD(TD_LCBR_RCTL), TD(TD_RCBR_RSFT),
+    LSFT_T(KC_GRV), KC_LCTL, LALT_T(KC_MINS), LGUI_T(KC_EQL), KC_NO,
+    KC_NO, RGUI_T(KC_QUOT), RALT_T(KC_BSLS), RCTL_T(KC_LBRC), RSFT_T(KC_RBRC),
 
-		KC_GRV, LGUI(KC_GRV), KC_MINS, KC_EQL, KC_NO,
-		KC_NO, KC_QUOT, KC_BSLS, KC_LBRC, KC_RBRC,
+    KC_TILD, LGUI(KC_GRV), KC_UNDS, KC_PLUS, KC_NO,
+    KC_NO, KC_DQUO, KC_PIPE, KC_LCBR, KC_RCBR,
 
 		KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS
@@ -89,127 +79,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS
 	)
-};
-
-void dance_tild_lsft_finished(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    register_code16(KC_TILD);
-  } else {
-    register_code(KC_LSFT);
-  }
-}
-
-void dance_tild_lsft_reset(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    unregister_code16(KC_TILD);
-  } else {
-    unregister_code(KC_LSFT);
-  }
-}
-
-
-void dance_unds_lalt_finished(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    register_code16(KC_UNDS);
-  } else {
-    register_code(KC_LALT);
-  }
-}
-
-void dance_unds_lalt_reset(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    unregister_code16(KC_UNDS);
-  } else {
-    unregister_code(KC_LALT);
-  }
-}
-
-void dance_plus_lgui_finished(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    register_code16(KC_PLUS);
-  } else {
-    register_code(KC_LGUI);
-  }
-}
-
-void dance_plus_lgui_reset(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    unregister_code16(KC_PLUS);
-  } else {
-    unregister_code(KC_LGUI);
-  }
-}
-
-void dance_dquo_rgui_finished(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    register_code16(KC_DQUO);
-  } else {
-    register_code(KC_RGUI);
-  }
-}
-
-void dance_dquo_rgui_reset(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    unregister_code16(KC_DQUO);
-  } else {
-    unregister_code(KC_RGUI);
-  }
-}
-
-void dance_pipe_ralt_finished(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    register_code16(KC_PIPE);
-  } else {
-    register_code(KC_RALT);
-  }
-}
-
-void dance_pipe_ralt_reset(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    unregister_code16(KC_PIPE);
-  } else {
-    unregister_code(KC_RALT);
-  }
-}
-
-void dance_lcbr_rctl_finished(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    register_code16(KC_LCBR);
-  } else {
-    register_code(KC_RCTL);
-  }
-}
-
-void dance_lcbr_rctl_reset(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    unregister_code16(KC_LCBR);
-  } else {
-    unregister_code(KC_RCTL);
-  }
-}
-
-void dance_rcbr_rsft_finished(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    register_code16(KC_RCBR);
-  } else {
-    register_code(KC_RSFT);
-  }
-}
-
-void dance_rcbr_rsft_reset(tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    unregister_code16(KC_RCBR);
-  } else {
-    unregister_code(KC_RSFT);
-  }
-}
-
-tap_dance_action_t tap_dance_actions[] = {
-  [TD_TILD_LSFT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_tild_lsft_finished, dance_tild_lsft_reset),
-  [TD_UNDS_LALT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_unds_lalt_finished, dance_unds_lalt_reset),
-  [TD_PLUS_LGUI] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_plus_lgui_finished, dance_plus_lgui_reset),
-  [TD_DQUO_RGUI] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_dquo_rgui_finished, dance_dquo_rgui_reset),
-  [TD_PIPE_RALT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_pipe_ralt_finished, dance_pipe_ralt_reset),
-  [TD_LCBR_RCTL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_lcbr_rctl_finished, dance_lcbr_rctl_reset),
-  [TD_RCBR_RSFT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_rcbr_rsft_finished, dance_rcbr_rsft_reset),
 };
